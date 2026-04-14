7eb7d4cb6ab780a69a08af5ee35c084dba292c32
Wa-ta-shi <245801539+Wa-ta-shi@users.noreply.github.com>
2026-04-14 21:33:14 +0800

    lab3 26/4/14

--- lab03/ex4_discrete_fn.s
+++ lab03/ex4_discrete_fn.s
@@ -1,11 +1,24 @@
 .globl f # this allows other files to find the function f
 
+.import utils.s
+
+.data
+nums:
+    .word 6, 61, 17, -38, 19, 42, 5
+
+.text
 # f takes in two arguments:
 # a0 is the value we want to evaluate f at
 # a1 is the address of the "output" array (read the lab spec for more information).
 # The return value should be stored in a0
 f:
     # Your code here
+    la t0 nums # t0 array address
+    addi t1 a0 3 # +3
+    slli t2 t1 2 # *4
+    add t0 t0 t2 # add offset
+
+    lw a0 0(t0) # load elements
 
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
--- lab03/ex5_factorial.s
+++ lab03/ex5_factorial.s
@@ -1,7 +1,7 @@
 .globl factorial
 
 .data
-n: .word 8
+n: .word 0
 
 .text
 # Don't worry about understanding the code in main
@@ -27,7 +27,19 @@ main:
 # The return value should be stored in a0
 factorial:
     # YOUR CODE HERE
-
+    li t1 1 # answer
+    li t2 0 # count
+    li t3 1 # current num
+    
+loop_start:
+    bge t2 a0 loop_end
+    mul t1 t1 t3 # answer *
+    addi t2 t2 1 # count +
+    addi t3 t3 1 # current num +
+    j loop_start
+    
+loop_end:
+    mv a0 t1 # give answer
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
     jr ra
